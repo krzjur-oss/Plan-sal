ba37fb7d3efca52f3a2964580cdc180e5aaf65f7
Krzysztof <krzjur@gmail.com>
2026-03-10 22:03:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // SalePlan Service Worker
-const CACHE = 'sp-v57';
+const CACHE = 'sp-v58';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
+  '/Plan-sal/',
+  '/Plan-sal/index.html',
+  '/Plan-sal/manifest.json',
+  '/Plan-sal/icon-192.png',
+  '/Plan-sal/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600&display=swap',
 ];
 
@@ -32,7 +32,7 @@ self.addEventListener('fetch', e => {
             caches.open(CACHE).then(c => c.put(e.request, r.clone()));
           }
           return r;
-        }).catch(() => caches.match('./index.html'));
+        }).catch(() => caches.match('/Plan-sal/index.html'));
       })
     );
     return;
